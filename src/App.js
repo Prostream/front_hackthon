0ebaf5969fc09efbd48c6f9843e6192fc64fcd1a
jimyh <jimyhacker@outlook.com>
2024-10-28 13:37:58 -0700

    first version of front pack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-import EnvironmentPage from './components/EnvironmentPage.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Forum from './components/Forum';
 
 function App() {
   return (
-    <div className="App">
-      <EnvironmentPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/forum" element={<Forum />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
